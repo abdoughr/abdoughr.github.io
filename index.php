5cc1a246e3545e7d5e913cc1f97e6eb09f8e6049
abdoughr <ghariani.abdelrahman@gmail.com>
2015-01-20 21:48:39 +0800

    commit initial repo

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Personal website</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <link type="text/css" rel="stylesheet" href="style.css"/>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
+  
+  </head>
+  
+ <body>
+ 
+ <div id="container">
+ 
+ <div id="header">
+
+ <div class="pic"></div>
+ 
+<div class="title">
+ <h1>Abdou Ghariani</h1>
+ <span>Ignorance, the root and stem of all evil.</span>
+    </div>
+
+ </div>
+     
+     <div class="dotted_line"></div>
+ 
+ <div id="main_content">
+ 
+ 
+ <div class="me">
+ 
+
+ <p>I am a <span class="lightblue">product designer</span> living in <span class="coral">Kuala Lumpur</span> and passionate about startups. I am currently working on my first mobile app Jumpa. I also co-crafted Agapesis. Previously I worked for Arthur D.Little, RadiumOne, Oximity and GroupM. Find me on Facebook, Twitter and Linkedin.</p>
+     
+    </div>
+   
+            
+<div class="myself">
+ 
+
+ <p>In my free time i like reading XIX french authors, spear fishing in South-East Asian beautiful spots and travelling to deep historical cities, digging in the crates to find some musical gems especially from middle-east and west-africa.</p>
+     
+    </div>
+     
+     
+<div class="metwo">
+     
+<p>I see myself as a crafter who likes to package beautiful services and products. It took me time to start creating things and execute my ideas but it finally happened and I am now busy bringing these ideas to life. I love new ideas and challenging the staus-quo. Drop me a line if you think I can help.</p>
+     
+     </div>
+ 
+ </div>
+
+ 
+ </div>
+ 
+ <div class="footer"></div>
+
+  </body>
+</html>
